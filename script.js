f09280427e35949e5e4d2408317a41ca7c377b36
FoggyCode <137562154+FoggyCode@users.noreply.github.com>
2025-11-02 21:05:48 +0100

    y

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Works!")
+
+const imprintLink = "https://www.foggyworks.com/visitedImprint"
+const privacyLink = "https://www.foggyworks.com/visitedPrivacyNotice"
+document.addEventListener("DOMContentLoaded" , function(){
+    if (document.title == "Imprint"){
+        console.log(document.title )
+        fetch(imprintLink).then(response => response.json()).then(data => {   })
+    }else if (document.title == "Privacy Notice") {
+        console.log(document.title )
+        fetch(privacyLink).then(response => response.json()).then(data => {   })
+    }
+
+})
